3bec942e0fff3a4f37527eb983d6784d1966c95a
19486 <feng.wang.03@jereh.cn>
2025-06-17 19:01:10 +0800

    推送

---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+<template>
+  <div>
+    <h1>this is a vue-project</h1>
+  </div>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
